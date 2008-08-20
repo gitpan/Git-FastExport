d1e3c8e396e25f3ad33a97c119c1f7d0dfad7edf
Philippe Bruhat (BooK) <book@cpan.org>
2008-08-20 20:19:53 +0000

    Import of BOOK/Git-FastExport-0.04 from CPAN.
    gitpan-cpan-distribution: Git-FastExport
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         BOOK/Git-FastExport-0.04.tar.gz
    gitpan-cpan-author:       BOOK
    gitpan-cpan-maturity:     released

---
+++ t/30stitch-repo.t
@@ -0,0 +1,111 @@
+use strict;
+use warnings;
+use Test::More;
+use File::Temp qw( tempdir );
+use IPC::Open2;
+use t::Utils;
+
+my @tests = (
+
+    # source repositories, refs, expected repository
+    # linear trees
+    [ 'A1 A2-A1 A3-A2', 'master=A3', 'A1 A2-A1 A3-A2' ],
+    [   'A1 A2-A1 A3-A2 B1 B2-B1 B3-B2',
+        'master=A3 master=B3',
+        'A1 A2-A1 A3-A2 B1-A3 B2-B1 B3-B2'
+    ],
+    [   'A1 B1 A2-A1 B2-B1 A3-A2 B3-B2',
+        'master=A3 master=B3',
+        'A1 B1-A1 A2-B1 B2-A2 A3-B2 B3-A3'
+    ],
+    [   'A1 B1 C1 A2-A1 B2-B1 C2-C1 A3-A2 B3-B2 C3-C2',
+        'master=A3 master=B3 master=C3',
+        'A1 B1-A1 C1-B1 A2-C1 B2-A2 C2-B2 A3-C2 B3-A3 C3-B3'
+    ],
+
+    # simple diamonds
+    [ 'A1 A2-A1 A3-A1 A4-A2A3', 'master=A4', 'A1 A2-A1 A3-A1 A4-A2A3' ],
+    [   'A1 A2-A1 A3-A1 A4-A2A3 B1 B2-B1 B3-B1 B4-B2B3',
+        'master=A4 master=B4',
+        'A1 A2-A1 A3-A1 A4-A2A3 B1-A4 B2-B1 B3-B1 B4-B2B3'
+    ],
+    [   'A1 B1 A2-A1 A3-A1 B2-B1 B3-B1 A4-A2A3 B4-B2B3',
+        'master=A4 master=B4',
+        'A1 B1-A1 A2-B1 B2-B1 A3-B2 B3-A2 A4-B3A3 B3-A4',
+        'The two master branches should be the same'
+    ],
+    [   'A1 B1 A2-A1 B2-B1 A3-A1 B3-B1 A4-A2A3 B4-B2B3',
+        'master=A4 master=B4',
+        'A1 B1-A1 A2-B1 B2-B1 A3-B2 B3-A2 A4-B3A3 B3-A4',
+        'The two master branches should be the same'
+    ],
+    [   'A1 B1 A2-A1 A3-A1 B2-B1 B3-B1 B4-B2B3 A4-A2A3 B5-B4 A5-A4',
+        'master=A5 master=B5',
+        'A1 B1-A1 A2-B1 B2-B1 A3-B2 B3-A2 B4-B3B2 B3-A4',
+        'The two master branches should be the same'
+    ],
+
+    # other trees
+    [   'A1 B1 A2-A1 B2-B1 A3-A2 A4-A2 B3-B2 B4-B2 A5-A4A3 B5-B3 B6-B4 B7-B6B5 B8-B7 A6-A5',
+        'master=A6 master=B8 topic=A3 topic=B5',
+        'A1 B1-A1 A2-B1 B2-A2 A3-B2 A4-B2 B3-A3 B4-A4 A5-B4B3 B5-A5 B6-B4 B7-B6B5 B8-B7 A6-B8',
+        'A6 should be attached to B8'
+    ],
+    [   'A1 B1 A2-A1 B2-B1 A3-A2 A4-A2 B3-B2 B4-B2 A5-A4A3 B5-B3 B6-B4 B7-B6B5 B8-B7 A6-A5 A7-A3 A8-A6',
+        'master=A8 master=B8 topic=A7 topic=B5',
+        'A1 B1-A1 A2-B1 B2-A2 A4-B2 B4-A4 B6-B4 A3-B2 B3-A3 A5-B4B3 B5-A5 B7-B5 B8-B7 A7-A5 A6-B8 A8-A6',
+        'The two master branches should be the same'
+    ],
+);
+
+# useful hack for quick testing
+@tests = grep {$_} @tests[@ARGV] if @ARGV;
+
+plan skip_all => 'No test selected' if !@tests;
+plan tests => scalar @tests;
+
+# the program we want to test
+my $gsr = File::Spec->rel2abs('script/git-stitch-repo');
+my $lib = File::Spec->rel2abs('lib');
+
+for my $t (@tests) {
+    my ( $src, $refs, $dst, $todo ) = @$t;
+
+    # a temporary directory for our tests
+    my $dir
+        = File::Spec->rel2abs( tempdir( 'git-XXXXX', CLEANUP => !@ARGV ) );
+
+    # create the source repositories
+    my @src = create_repos( $dir => $src, $refs );
+
+    # create the destination repository
+    my $repo = new_repo( $dir => 'RESULT' );
+
+    # run git-stitch-repo on the source repositories
+    my ( $in, $out );
+    my $pid
+        = open2( $out, $in, $^X, "-I$lib", $gsr, map { $_->wc_path } @src );
+
+    # run git-fast-import on the destination repository
+    my ( $fh, $c ) = $repo->command_input_pipe( 'fast-import', '--quiet' );
+
+    # pipe the output of git-stitch-repo into git-fast-import
+    while (<$out>) {
+        next if /^progress /;    # ignore progress info
+        print {$fh} $_;
+    }
+    $repo->command_close_pipe( $fh, $c );
+
+    # get the description of the resulting repository
+    my $result = repo_description($repo);
+    if ($todo) {
+    TODO: {
+            local $TODO = $todo;
+            is( $result, $dst, "$src => $dst" );
+        }
+    }
+    else {
+        is( $result, $dst, "$src => $dst" );
+    }
+}
+
---
+++ t/Utils.pm
@@ -0,0 +1,231 @@
+use strict;
+use warnings;
+use File::Path;
+use File::Spec;
+use Cwd;
+use Git;
+
+# some data for the file content
+my @data = <DATA>;
+my $idx = 0;
+
+1;
+
+# create a new, empty repository
+sub new_repo {
+    my ( $dir, $name ) = @_;
+    my $cwd = getcwd;
+
+    # alas, this can't be done with Git.pm
+    my $wc = File::Spec->rel2abs( File::Spec->catfile( $dir, $name ) );
+    mkpath $wc;
+    chdir $wc;
+    `git-init`;
+    chdir $cwd;
+    return Git->repository( Directory => $wc );
+}
+
+# produce a text description of a given repository
+sub repo_description {
+    my ($repo) = @_;
+    my %log;    # map sha1 to log message
+    my @commits;
+
+    # process the whole tree
+    my ( $fh, $c )
+        = $repo->command_output_pipe( 'log', '--pretty=format:%H-%P-%s',
+        '--date-order', '--all' );
+    while (<$fh>) {
+        chomp;
+        my ( $h, $p, $log ) = split /-/, $_, 3;
+        $log{$h} = $log;
+        $p =~ y/ //d;
+        push @commits, $p ? "$log-$p" : $log;
+    }
+    $repo->command_close_pipe( $fh, $c );
+
+    # replace SHA-1 by log name
+    my $desc = join ' ', reverse @commits;
+    $desc =~ s/(\w{40})/$log{$1}/g;
+
+    return $desc;
+}
+
+# create a set of repositories from a given description
+sub create_repos {
+    my ( $dir, $desc, $refs ) = @_;
+    my $info = { dir => $dir, repo => {}, sha1 => {} };
+
+    for my $commit ( split / /, $desc ) {
+        my ( $child, $parent ) = split /-/, $commit;
+        my @child = $child =~ /([A-Z]\d+)/g;
+        my @parent = $parent =~ /([A-Z]\d+)/g if $parent;
+
+        die "bad node description" if @child > 1 && @parent > 1;
+
+        if ( @child > 1 ) {    # branch point
+            create_linear_commit( $info, $_, $parent[0] ) for @child;
+        }
+        elsif ( @parent > 1 ) {    # merge point
+            create_merge_commit( $info, $child[0], @parent );
+        }
+        else {                     # simple, linear commit
+            create_linear_commit( $info, $child[0], $parent[0] );
+        }
+        sleep 1;
+    }
+
+    # setup the refs (branches & tags)
+    for my $ref ( split / /, $refs ) {
+        my ( $name, $type, $commit ) = split /([>=])/, $ref;
+        my $repo = $info->{repo}{ substr( $commit, 0, 1 ) };
+        if ( $type eq '=' ) {      # branch
+            $repo->command( 'branch', '-D', $name )
+                if grep {/^..$name$/} $repo->command('branch');
+            $repo->command( 'branch', $name, $info->{sha1}{$commit} );
+        }
+        else {                     # tag
+            $repo->command( 'tag', $name, $info->{sha1}{$commit} );
+        }
+    }
+
+    # return the repository objects
+    return map { $info->{repo}{$_} } sort keys %{ $info->{repo} };
+}
+
+sub create_linear_commit {
+    my ( $info, $child, $parent ) = @_;
+    my $name = substr( $child, 0, 1 );
+
+    # create the repo if needed
+    my $repo = $info->{repo}{$name};
+    if ( !$repo ) {
+        $repo = $info->{repo}{$name} = new_repo( $info->{dir} => $name );
+    }
+
+    # checkout the parent commit
+    $repo->command( 'checkout', '-q', $info->{sha1}{$parent} ) if $parent;
+    my $base = File::Spec->catfile( $info->{dir}, $name );
+    update_file( $base, $name );
+    $repo->command( 'add', $name );
+    $repo->command( 'commit', '-m', $child );
+    $info->{sha1}{$child}
+        = $repo->command_oneline(qw( log -n 1 --pretty=format:%H HEAD ));
+}
+
+sub create_merge_commit {
+    my ( $info, $child, @parents ) = @_;
+    my $name = substr( $child, 0, 1 );
+    my $repo = $info->{repo}{$name};
+
+    # checkout the first parent
+    my $parent = shift @parents;
+    $repo->command( 'checkout', '-q', $info->{sha1}{$parent} );
+
+    # merge the other parents
+    eval {
+        $repo->command_noisy( 'merge', '-n',
+            map { $info->{sha1}{$_} } @parents,
+        );
+        1;
+        }
+        or do {
+        my $base = File::Spec->catfile( $info->{dir}, $name );
+        update_file( $base, $name );
+        $repo->command( 'add', $name );
+        $repo->command( 'commit', '-m', $child );
+        };
+    $info->{sha1}{$child}
+        = $repo->command_oneline(qw( log -n 1 --pretty=format:%H HEAD ));
+}
+
+sub update_file {
+    my ($file) = File::Spec->catfile(@_);
+    open my $fh, '>', $file or die "Can't open $file: $!";
+    print $fh $data[ $idx++ % @data ];
+    close $fh;
+}
+
+__DATA__
+aieee
+aiieee
+awk
+awkkkkkk
+bam
+bang
+bang_eth
+bap
+biff
+bloop
+blurp
+boff
+bonk
+clange
+clank
+clank_est
+clash
+clunk
+clunk_eth
+crash
+crr_aaack
+crraack
+cr_r_a_a_ck
+crunch
+crunch_eth
+eee_yow
+flrbbbbb
+glipp
+glurpp
+kapow
+kayo
+ker_plop
+ker_sploosh
+klonk
+krunch
+ooooff
+ouch
+ouch_eth
+owww
+pam
+plop
+pow
+powie
+qunckkk
+rakkk
+rip
+slosh
+sock
+spla_a_t
+splatt
+sploosh
+swa_a_p
+swish
+swoosh
+thunk
+thwack
+thwacke
+thwape
+thwapp
+touche
+uggh
+urkk
+urkkk
+vronk
+whack
+whack_eth
+wham_eth
+whamm
+whap
+zam
+zamm
+zap
+zapeth
+zgruppp
+zlonk
+zlopp
+zlott
+zok
+zowie
+zwapp
+z_zwap
+zzzzzwap
